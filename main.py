3db5dee326c5acc39f169eecb727d3a5ab7a3049
zalphee <zlaphee@gmail.com>
2022-03-27 00:46:52 +0900

    첫 번째 커밋... 셋팅...

---
+++ main.py
@@ -0,0 +1,45 @@
+import pygame, sys
+
+# General setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setting up the main window
+screen_width = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Pong')
+
+while True:
+    # Handling input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Updateing the window
+    pygame.display.flip()
+    clock.tick(60)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
